2d72d2063e896dc420f76c407e530c570da5e185
Tumelo Mogono <tumelomogono@gmail.com>
2024-04-25 16:57:48 +0200

    removed unused function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TASK: import helper functions from utils
 // TASK: import initialData
-import { getTasks, createNewTask, patchTask, putTask, deleteTask } from './utils/taskFunctions.js';
+import { getTasks, createNewTask, patchTask, deleteTask } from './utils/taskFunctions.js';
 import { initialData } from './initialData.js';
 
 /*************************************************************************************************************************************************
